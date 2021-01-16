ed93bd778555a3dcce3597b806fb104c2464c687
HORRIBLE12 <albert_destroyer@protonmail.com>
2021-01-16 15:23:18 +0000

    done

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var name = "Alice";
 
 //only change code below this line
 
-var firstChar; //get the first character of the name
+var firstChar="A" //get the first character of the name
 
-var middleChar; //get the middle character of the name
+var middleChar="i" //get the middle character of the name
 
-var lastChar; //get the last character of the name
+var lastChar="e" //get the last character of the name
 
 //only change code above this line
 
