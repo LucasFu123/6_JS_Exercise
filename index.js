eb9de1c13387919b6514af137ed7771c693b0e8f
ozubergz <34900230+ozubergz@users.noreply.github.com>
2021-01-21 14:16:09 -0500

    use the `slice` method
    You were supposed to use the slice method to extract the characters from the string value.

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var name = "Alice";
 
 //only change code below this line
 
-var firstChar="A" //get the first character of the name
+var firstChar = name.slice(0, 1) //get the first character of the name
 
-var middleChar="i" //get the middle character of the name
+var middleChar = name.slice(2, 3) //get the middle character of the name
 
-var lastChar="e" //get the last character of the name
+var lastChar = name.slice(name.length - 1) //get the last character of the name
 
 //only change code above this line
 
